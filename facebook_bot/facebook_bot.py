f01e89b94e7668009959aea6dab61cbed64135af
tu <tu0703@gmail.com>
2017-05-09 12:20:07 +0700

    fix bugs

--- facebook_bot/facebook_bot.py
+++ facebook_bot/facebook_bot.py
@@ -133,7 +133,7 @@ def get_events_by_location(latitude, longitude, place_type='*',
 
     for point in generate_coordinate(*CIRCLE, scan_radius=scan_radius):
         page_list = get_page_ids(latitude=point[0], longitude=point[1],
-                                 place_type=place_type, distance=distance,
+                                 query_agrument=place_type, distance=distance,
                                  limit=limit)
         for page_id in page_list:
             nodes = get_events(page_id, base_time=base_time,
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python-facebook-bot',
-      version='0.1.4',
+      version='0.1.6',
       description='Using API to get Facebook Events by location, etc... with Python',
       url='http://github.com/tudoanh/python-facebook-bot',
       author='Do Anh Tu',
